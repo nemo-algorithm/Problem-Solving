8924c72145422db1131e9c179ba90128fdd9f4f7
nemo <nemo-algorithm@korea.ac.kr>
2020-07-31 16:55:58 +0900

    Update 7469 faster (n lg n + m lg n)

--- boj/#7469 K번째 수.cpp
+++ boj/#7469 K번째 수.cpp
@@ -8,7 +8,7 @@ struct PST {
         while (idx_max - idx_min + 1 >= two_n) n++, two_n *= 2;
         root.resize(Q + 1);
         seg.resize(max(1LL, n * Q));
-        q = 0, tn = 0;
+        q = 0, tn = Q;
         seg[0] = {0, 0, 0};
     }
     ll idx_min, idx_max;
@@ -33,9 +33,9 @@ struct PST {
             seg[a].req = seg[seg[a].left_node].req + seg[seg[a].right_node].req;
         }
     }
-    void update(ll s, ll d) {
-        root[++q] = ++tn;
-        update(root[q - 1], root[q], idx_min, idx_max, s, d);
+    void update(ll i, ll s, ll d) {
+        root[i] = i;
+        update(i - 1, i, idx_min, idx_max, s, d);
     }
     ll query(int a, ll l, ll r, ll s, ll d) {
         if (a == 0) return 0;
@@ -46,22 +46,20 @@ struct PST {
     ll query(int a, int s, int d) {
         return query(root[a], idx_min, idx_max, s, d);
     }
+    int kth(int a, int b, ll l, ll r, int k) {
+        if(l == r) return l;
+        int val = seg[seg[b].left_node].req - seg[seg[a].left_node].req;
+        if(val >= k) return kth(seg[a].left_node, seg[b].left_node, l, (l + r) / 2, k);
+        else return kth(seg[a].right_node, seg[b].right_node, (l + r) / 2 + 1, r, k - val);
+    }
+    int kth(int a, int b, int k) {
+        return kth(a-1, b, idx_min, idx_max, k);
+    }
 } pst;
 int n, m;
-struct st{
-    int val, idx, ord;
-} arr[1<<17];
+struct st{ int val, idx, ord = 0; } arr[1<<17];
 ll o2v[1<<17];
-int query(int a, int b, int k) {
-    int l=1, r=n, ret=1e9;
-    while(l <= r) {
-        int mid = (l + r) / 2;
-        int val = pst.query(mid, a, b);
-        if(val < k) l = mid + 1;
-        else r = mid - 1, ret = min(ret, mid);
-    }
-    return ret;
-}
+
 int main() {
     ios_base::sync_with_stdio(0); cin.tie(0);
     cin>>n>>m;
@@ -70,12 +68,15 @@ int main() {
         return a.val < b.val;
     });
     for(int i=1; i<=n; i++) arr[i].ord = i, o2v[i] = arr[i].val;
+    sort(arr+1, arr+n+1, [&](st a, st b){
+        return a.idx < b.idx;
+    });
     pst = PST(n, 1, n);
     for(int i=1; i<=n; i++) {
-        pst.update(arr[i].idx, 1);
+        pst.update(i, arr[i].ord, 1);
     }
     for(int i=1; i<=m; i++) {
         int a, b, k; cin>>a>>b>>k;
-        cout<<o2v[query(a, b, k)]<<'\n';
+        cout<<o2v[pst.kth(a, b, k)]<<'\n';
     }
 }
